3fed07e46241ca5cae73cb80342de4da2fef419e
Baron Wilhelm Stein <johnfreeman16561@gmail.com>
2018-12-24 20:48:54 +0200

    actionPage start

--- App/src/client/components/ActionPage.js
+++ App/src/client/components/ActionPage.js
@@ -3,11 +3,16 @@ import '../css/ActionPage.css';
 import Header from './Header';
 
 export default class ActionPage extends Component {
+
+    constructor(props) {
+        this.state = { accountType: props.accountType };
+    }
+
     render() {
         return (
             <div>
                 <Header/>
-                <ActionList/>
+                <ActionList accountType={this.state.accountType}/>
             </div>
         );
     }
@@ -15,11 +20,47 @@ export default class ActionPage extends Component {
 
 class ActionList extends Component {
 
+    constructor(props) {
+        this.state = { accountType: props.accountType };
+    }
+
     render() {
-        <div className="grid">
-            <ul>
-                
-            </ul>
-        </div>
+
+        switch(this.state.accountType)
+        {
+            case "stud":
+                return (
+                    <div className="stud-action-bar">
+                        <button>
+                            
+                        </button>
+                    </div>
+                );
+                break;
+
+            case "publ":
+                return (
+                    <div className="publ-action-bar">
+
+                    </div>
+                );
+                break;
+
+            case "secr":
+                return (
+                    <div className="secr-action-bar">
+
+                    </div>
+                );
+                break;
+            
+            case "dist":
+                return (
+                    <div className="dist-action-bar">
+
+                    </div>
+                );
+                break;
+        }
     }
 }
